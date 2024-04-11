3c3e705426f39f551435666e241b5ce55841bdd1
devg211 <guptadev599@gmail.com>
2024-04-11 22:32:59 +0530

    commit

--- src/textSummarization/config/configuration.py
+++ src/textSummarization/config/configuration.py
@@ -1,5 +1,5 @@
 from textSummarization.constants import *
-from textSummarization.entity import DataIngestionConfig
+from textSummarization.entity import DataIngestionConfig, DataValidationConfig
 from textSummarization.utils.common import create_directories, read_yaml
 
 
@@ -29,3 +29,17 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
+    
